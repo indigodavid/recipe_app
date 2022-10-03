e846e96b13e59bcd7863e2a30a092de70908c5fd
Addisu Haile Tedla <addisu.haile@yahoo.com>
2022-10-03 22:22:05 +0300

    Modify the stylesheet

--- app/javascript/application.js
+++ app/javascript/application.js
@@ -1,6 +1,3 @@
 // Configure your import map in config/importmap.rb. Read more: https://github.com/rails/importmap-rails
-//= require jquery3
-//= require popper
-//= require bootstrap-sprockets
 import "@hotwired/turbo-rails"
 import "controllers"
