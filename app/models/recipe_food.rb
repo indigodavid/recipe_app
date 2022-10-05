abb62dd1c975b5c21a9b3c397824485827ad971e
David Vera Castillo <indigodavid@gmail.com>
2022-10-05 15:58:48 -0500

    Add methods for handling shopping list on model

--- app/models/recipe_food.rb
+++ app/models/recipe_food.rb
@@ -2,4 +2,15 @@ class RecipeFood < ApplicationRecord
   belongs_to :recipe
   belongs_to :food
   validates :quantity, numericality: { only_integer: true, greater_than_or_equal_to: 0 }
+
+  def quantity_needed(user_food)
+    return quantity unless user_food
+
+    diff = user_food.quantity - food.quantity
+    diff.negative? ? -diff : 0
+  end
+
+  def cost_required(user_food)
+    (quantity_needed(user_food) * food.price).round(2)
+  end
 end
